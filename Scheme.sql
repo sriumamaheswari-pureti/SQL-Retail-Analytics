594df4ef8184e852c42a1088af4fa5afb17d8f3c
sriumamaheswari-pureti <umapureti84@gmail.com>
2026-03-31 09:17:19 +0530

    Create initial database schema with tables

--- Scheme.sql
+++ Scheme.sql
@@ -1,30 +1,30 @@
--- ============================================
+
 -- Create Customers Table
--- ============================================
+
 CREATE TABLE customers (
     customer_id   NUMBER,
     customer_name VARCHAR2(50)
 );
 
--- ============================================
+
 -- Create Stores Table
--- ============================================
+
 CREATE TABLE stores (
     store_id   NUMBER,
     store_name VARCHAR2(50)
 );
 
--- ============================================
+
 -- Create Products Table
--- ============================================
+
 CREATE TABLE products (
     product_id NUMBER,
     category   VARCHAR2(50)
 );
 
--- ============================================
+
 -- Create Sales Table
--- ============================================
+
 CREATE TABLE sales (
     sale_id     NUMBER,
     customer_id NUMBER,
